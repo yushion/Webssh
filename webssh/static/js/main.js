76e53c844f017dcd281f91b156b9ddfd3b647aec
yushion <97763838@QQ.com>
2024-09-23 05:12:32 +0800

    Update main.js

--- webssh/static/js/main.js
+++ webssh/static/js/main.js
@@ -80,7 +80,7 @@ jQuery(function($){
       state = DISCONNECTED,
       messages = {1: 'This client is connecting ...', 2: 'This client is already connnected.'},
       key_max_size = 16384,
-      fields = ['hostname', 'port', 'username', 'initcommand'],
+      fields = ['hostname', 'port', 'username'],
       form_keys = fields.concat(['password', 'totp']),
       opts_keys = ['bgcolor', 'title', 'encoding', 'initcommand', 'term', 'fontsize', 'fontcolor', 'cursor'],
       url_form_data = {},
